b733acc045231b6341e781a2d36ddb55b46a8168
jonysins2211 <Jonysins11220@gmail.com>
2025-06-05 00:43:22 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -216,7 +216,7 @@ def run_health_server():
 threading.Thread(target=run_health_server, daemon=True).start()
 
 async def main():
-    await bot.start()
+    await bot.start()
     print("✅ Bot is up and running...")
     await idle()  # <-- Keeps the bot alive and responsive
 
